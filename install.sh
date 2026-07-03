fdc4804cedb892e97dbc3af165af76410cb02a8c
4kercc <4kercc@users.noreply.github.com>
2026-07-03 16:41:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -133,20 +133,20 @@ install_V2bX() {
 
     if  [ $# == 0 ] ;then
         # 使用q42602736的V2BX-malio仓库
-        last_version=$(curl -Ls "https://api.github.com/repos/q42602736/V2BX-malio/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/4kercc/V2BX-malio/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}检测 V2bX-malio 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 V2bX 版本安装${plain}"
             exit 1
         fi
         echo -e "检测到 V2bX-malio 最新版本：${last_version}，开始安装"
-        wget -q -N --no-check-certificate -O /usr/local/V2bX/V2bX-linux.zip https://github.com/q42602736/V2BX-malio/releases/download/${last_version}/V2bX-linux-${arch}.zip
+        wget -q -N --no-check-certificate -O /usr/local/V2bX/V2bX-linux.zip https://github.com/4kercc/V2BX-malio/releases/download/${last_version}/V2bX-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 V2bX-malio 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/q42602736/V2BX-malio/releases/download/${last_version}/V2bX-linux-${arch}.zip"
+        url="https://github.com/4kercc/V2BX-malio/releases/download/${last_version}/V2bX-linux-${arch}.zip"
         echo -e "开始安装 V2bX-malio $1"
         wget -q -N --no-check-certificate -O /usr/local/V2bX/V2bX-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
@@ -255,15 +255,7 @@ EOF
     echo "V2bX version      - 查看 V2bX 版本"
     echo "------------------------------------------"
     # 首次安装询问是否生成配置文件
-    if [[ $first_install == true ]]; then
-        read -rp "检测到你为第一次安装V2bX,是否自动直接生成配置文件？(y/n): " if_generate
-        if [[ $if_generate == [Yy] ]]; then
-            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/wyx2685/V2bX-script/master/initconfig.sh
-            source initconfig.sh
-            rm initconfig.sh -f
-            generate_config_file
-        fi
-    fi
+
 }
 
 echo -e "${green}开始安装${plain}"
